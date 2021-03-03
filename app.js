0f809878c3c5a5bee58ee3a8a71faec51033a7aa
Isadora Decker-Lucke <isadoradl5@gmail.com>
2021-03-02 21:16:37 -0700

    updated sendfile and url

--- app.js
+++ app.js
@@ -37,12 +37,12 @@ app.use("/users", userRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "frontend/build")));
   app.get("*", (req, res) => {
-    res.sendFile("index.html", { root: "/frontend/public" });
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 }
 
 app.get("*", (req, res) => {
-  res.sendFile("index.html", { root: "/frontend/build" });
+  res.sendFile(path.resolve(__dirname, "frontend", "public", "index.html"));
 });
 
 /** 404 handler */
