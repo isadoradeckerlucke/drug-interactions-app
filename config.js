2d08f329656a818c50324461c8dd2814daed00e3
Isadora Decker-Lucke <isadoradl5@gmail.com>
2021-03-02 20:02:27 -0700

    updated port

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 const SECRET_KEY = process.env.SECRET_KEY || "test";
 
-const PORT = +process.env.PORT || 3000;
+const PORT = +process.env.PORT || 5000;
 
 // database is:
 //
