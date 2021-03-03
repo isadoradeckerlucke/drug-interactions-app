cd52d4c6f4b8c1c9d37da989de2d81c85c9d0c91
Isadora Decker-Lucke <isadoradl5@gmail.com>
2021-03-02 22:06:15 -0700

    added error catching

--- models/user.js
+++ models/user.js
@@ -21,7 +21,6 @@ class User {
         return { invalid: "invalid username / password" };
       }
     }
-    throw new ExpressError("Invalid username/password", 401);
   }
 
   static async signup({ username, password, email }) {
@@ -49,85 +48,114 @@ class User {
 
   static async update(username, data) {
     /**partially update a user */
-    if (data.password) {
-      data.password = await bcrypt.hash(data.password, BCRYPT_WORK_FACTOR);
+    try {
+      if (data.password) {
+        data.password = await bcrypt.hash(data.password, BCRYPT_WORK_FACTOR);
+      }
+
+      const { setCols, values } = sqlPartialUpdate(data, {
+        username: "username",
+        email: "email",
+      });
+      const usernameVarIdx = "$" + (values.length + 1);
+
+      const querySql = `UPDATE users SET ${setCols} WHERE username = ${usernameVarIdx} RETURNING username, email`;
+      const result = await db.query(querySql, [...values, username]);
+      const user = result.rows[0];
+      if (!user)
+        throw new ExpressError(
+          `there is no user with username ${username}`,
+          404
+        );
+      delete user.password;
+      return user;
+    } catch (err) {
+      console.log(err);
     }
-
-    const { setCols, values } = sqlPartialUpdate(data, {
-      username: "username",
-      email: "email",
-    });
-    const usernameVarIdx = "$" + (values.length + 1);
-
-    const querySql = `UPDATE users SET ${setCols} WHERE username = ${usernameVarIdx} RETURNING username, email`;
-    const result = await db.query(querySql, [...values, username]);
-    const user = result.rows[0];
-    if (!user)
-      throw new ExpressError(`there is no user with username ${username}`, 404);
-    delete user.password;
-    return user;
   }
 
   static async remove(username) {
     /**delete a user */
-    let result = await db.query(
-      `DELETE FROM users WHERE username = $1 RETURNING username`,
-      [username]
-    );
-    const user = result.rows[0];
-    if (!user)
-      throw new ExpressError(`there is no user with username ${username}`, 404);
+    try {
+      let result = await db.query(
+        `DELETE FROM users WHERE username = $1 RETURNING username`,
+        [username]
+      );
+      const user = result.rows[0];
+      if (!user)
+        throw new ExpressError(
+          `there is no user with username ${username}`,
+          404
+        );
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   static async get(username) {
-    const userRes = await db.query(
-      `SELECT username, email FROM users WHERE username = $1`,
-      [username]
-    );
-    const user = userRes.rows[0];
+    try {
+      const userRes = await db.query(
+        `SELECT username, email FROM users WHERE username = $1`,
+        [username]
+      );
+      const user = userRes.rows[0];
 
-    if (!user)
-      throw new ExpressError(`there is no user with username ${username}`, 404);
+      if (!user)
+        throw new ExpressError(
+          `there is no user with username ${username}`,
+          404
+        );
 
-    const userSavedDrugs = await db.query(
-      `SELECT ud.med_name FROM users_drugs_relation AS ud WHERE ud.username = $1`,
-      [username]
-    );
+      const userSavedDrugs = await db.query(
+        `SELECT ud.med_name FROM users_drugs_relation AS ud WHERE ud.username = $1`,
+        [username]
+      );
 
-    user.savedDrugs = userSavedDrugs.rows.map((ud) => ud.med_name);
-    return user;
+      user.savedDrugs = userSavedDrugs.rows.map((ud) => ud.med_name);
+      return user;
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   static async saveDrug(username, med_name) {
-    const preCheckUsers = await db.query(
-      `SELECT username FROM users WHERE username = $1`,
-      [username]
-    );
-    const user = preCheckUsers.rows[0];
-    if (!user)
-      throw new ExpressError(
-        `there is no user with username ${username} in the database`,
-        404
+    try {
+      const preCheckUsers = await db.query(
+        `SELECT username FROM users WHERE username = $1`,
+        [username]
       );
+      const user = preCheckUsers.rows[0];
+      if (!user)
+        throw new ExpressError(
+          `there is no user with username ${username} in the database`,
+          404
+        );
 
-    await db.query(
-      `INSERT INTO users_drugs_relation (med_name, username) VALUES ($1, $2)`,
-      [med_name, username]
-    );
+      await db.query(
+        `INSERT INTO users_drugs_relation (med_name, username) VALUES ($1, $2)`,
+        [med_name, username]
+      );
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   static async unsaveDrug(username, med_name) {
     /** unsave a drug */
-    let result = await db.query(
-      `DELETE FROM users_drugs_relation WHERE med_name = $1 AND username = $2 RETURNING med_name`,
-      [med_name, username]
-    );
-    const medName = result.rows[0];
-    if (!medName)
-      throw new ExpressError(
-        `there is no drug ${medName} saved for ${username}`,
-        404
+    try {
+      let result = await db.query(
+        `DELETE FROM users_drugs_relation WHERE med_name = $1 AND username = $2 RETURNING med_name`,
+        [med_name, username]
       );
+      const medName = result.rows[0];
+      if (!medName)
+        throw new ExpressError(
+          `there is no drug ${medName} saved for ${username}`,
+          404
+        );
+    } catch (err) {
+      console.log(err);
+    }
   }
 }
 
