e8a81103a46da47ff2c57cb38e181898eb882bca
Isadora Decker-Lucke <isadoradl5@gmail.com>
2021-03-02 23:58:40 -0700

    update proxy

--- frontend/src/Api.js
+++ frontend/src/Api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:5000/api";
+console.log("******", process.env.REACT_APP_BASE_URL, "base url***********");
 
 class DrugInteractionApi {
   static async request(endpoint, data = {}, method = "get") {
