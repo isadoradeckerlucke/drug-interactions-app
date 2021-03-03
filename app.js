9955bfbe634aea60f685e6699d06bee9b16f11a5
Isadora Decker-Lucke <isadoradl5@gmail.com>
2021-03-02 21:45:53 -0700

    edited send file

--- app.js
+++ app.js
@@ -30,15 +30,11 @@ app.use((req, res, next) => {
 app.use("/interactions", drugRoutes);
 app.use("/users", userRoutes);
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
-// });
-
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "frontend/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  // });
 }
 
 app.get("*", (req, res) => {
